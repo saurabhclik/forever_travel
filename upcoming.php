6510669871634b11f7745ad9861a23c38fad48d9
saurabhclik <saurabhrawat@clikzopinnovations.com>
2026-04-30 10:21:23 +0530

    fixed db and upcoming

---
+++ upcoming.php
@@ -0,0 +1,12 @@
+1. Query page should show all status and removed un used status also add missed followup filter
+
+2. Show agent scoring in dashboard
+
+3. show status in query in raw, show lead id as well
+
+4. Call and date should time mandatory in followup 
+
+5. before sale and expense amount, lead should not go in converted section, if somebody change the status to converted and the sale or the expense amount is missing, show popup so enter sale and expense amount
+
+6. When we are Collecting payment, error is coming Query ID is missing, and when we are converting sale, i have entered sale amount but not showing in sale amount when we click on Sale amount... check error in group
+
